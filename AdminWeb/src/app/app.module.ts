5b70a81061e1abf76e099f8ad115fa2c936b4f7c
David Paller <dpaller94@gmail.com>
2018-09-30 21:57:52 -0400

    Start admin angular site

--- AdminWeb/src/app/app.module.ts
+++ AdminWeb/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes : Routes = [
+  { path: "Home", component: HomeComponent},
+
+  // Default
+  { path: '', redirectTo: "/Home", pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
