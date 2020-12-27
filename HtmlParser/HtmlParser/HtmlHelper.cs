1bbc9bb42e2de456570095ab03bb8e8eb17ea2c8
David Paller <darthdollar@gmail.com>
2020-12-27 15:59:33 -0500

    Add html parser

---
+++ HtmlParser/HtmlParser/HtmlHelper.cs
@@ -0,0 +1,33 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlParser
+{
+    internal class HtmlHelper
+    {
+        private IBrowsingContext _browsingContext;
+        private IConfiguration _config;
+        private IDocument _document;
+
+        public HtmlHelper(string html, IConfiguration configuration = null)
+        {
+            _config = configuration ?? Configuration.Default;
+            _browsingContext = BrowsingContext.New(_config);
+
+            _document = _browsingContext.OpenAsync(x => x.Content(html)).Result;
+        }
+
+        internal void ReplaceContnent(string id, string value)
+        {
+            _document.QuerySelector("#" + id).TextContent = value;
+        }
+
+        internal string GetHtml()
+        {
+            return _document.ToHtml();
+        }
+    }
+}
---
+++ HtmlParser/HtmlParser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace HtmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = LoadHtmlFile();
+            var htmlHelper = new HtmlHelper(html);
+
+            htmlHelper.ReplaceContnent("address", "3091 countryside dr");
+
+            Console.WriteLine(htmlHelper.GetHtml());
+            Console.ReadLine();
+        }
+
+        private static string LoadHtmlFile()
+        {
+            var file = new FileInfo("TestPage.html");
+
+            using var reader = file.OpenText();
+
+            return reader.ReadToEnd();
+        }
+    }
+}
